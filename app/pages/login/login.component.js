3b28d4708afe301e4322879b312d72a03214f710
Thomas Walker <t.d.walker42@gmail.com>
2016-11-30 21:23:27 +0000

    Routing implemented, allowing the login page to take you to the list page

---
+++ app/pages/login/login.component.js
@@ -0,0 +1,52 @@
+"use strict";
+var core_1 = require("@angular/core");
+var user_1 = require("../../shared/user/user");
+var user_service_1 = require("../../shared/user/user.service");
+var router_1 = require("@angular/router");
+var LoginComponent = (function () {
+    function LoginComponent(router, userService) {
+        this.router = router;
+        this.userService = userService;
+        this.isLoggingIn = true;
+        this.user = new user_1.User();
+        // Default account credentials for faster testing
+        this.user.email = "user@nativescript.org";
+        this.user.password = "password";
+    }
+    LoginComponent.prototype.submit = function () {
+        if (this.isLoggingIn) {
+            this.login();
+        }
+        else {
+            this.signUp();
+        }
+    };
+    LoginComponent.prototype.login = function () {
+        var _this = this;
+        this.userService.login(this.user)
+            .subscribe(function () { return _this.router.navigate(["/list"]); }, function (error) { return alert("Unfortunately we could not find your account."); });
+    };
+    LoginComponent.prototype.signUp = function () {
+        var _this = this;
+        this.userService.register(this.user)
+            .subscribe(function () {
+            alert("Your account was successfully created.");
+            _this.toggleDisplay();
+        }, function () { return alert("Unfortunately we were unable to create your account."); });
+    };
+    LoginComponent.prototype.toggleDisplay = function () {
+        this.isLoggingIn = !this.isLoggingIn;
+    };
+    LoginComponent = __decorate([
+        core_1.Component({
+            selector: "my-app",
+            providers: [user_service_1.UserService],
+            templateUrl: "pages/login/login.html",
+            styleUrls: ["pages/login/login-common.css", "pages/login/login.css"],
+        }), 
+        __metadata('design:paramtypes', [router_1.Router, user_service_1.UserService])
+    ], LoginComponent);
+    return LoginComponent;
+}());
+exports.LoginComponent = LoginComponent;
+//# sourceMappingURL=login.component.js.map
---
+++ platforms/ios/build/emulator/CarWashApp.app/app/app.component.js
@@ -0,0 +1,17 @@
+"use strict";
+var core_1 = require("@angular/core");
+var AppComponent = (function () {
+    function AppComponent() {
+    }
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: "my-app",
+            template: "\n    <StackLayout>\n      <Image src=\"res://logo_login\" stretch=\"none\" horizontalAlignment=\"center\"></Image>\n      <TextField hint=\"Email Address\" keyboardType=\"email\"\n        autocorrect=\"false\" autocapitalizationType=\"none\"></TextField>\n      <TextField hint=\"Password\" secure=\"true\"></TextField>\n\n      <Button text=\"Sign in\" class=\"submit-button\"></Button>\n      <Button text=\"Sign up for Groceries\"></Button>\n    </StackLayout>\n  ",
+            styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
